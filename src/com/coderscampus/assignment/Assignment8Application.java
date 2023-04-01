be007faa37fc7b2aace541b3f67262bfb6e52c75
Elizabeth <namelesselizabeth@gmail.com>
2023-04-01 08:50:49 -0700

    v2
    Added step 2

--- src/com/coderscampus/assignment/Assignment8Application.java
+++ src/com/coderscampus/assignment/Assignment8Application.java
@@ -1,7 +1,10 @@
 package com.coderscampus.assignment;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,13 +13,19 @@ public class Assignment8Application {
 
 	public static void main(String[] args) {
 		ExecutorService service = Executors.newCachedThreadPool();
-		List<Integer> outputList = new ArrayList<>();
+		
+		List<Integer> outputList = Collections.synchronizedList( new ArrayList<>() );
+		
 		List<CompletableFuture<Void>> tasks = new ArrayList<>();
 		Assignment8 assignment = new Assignment8();
-
-		for(int i=0; i< 1000; i++) {
+		
+		synchronized(outputList) {
+			
+		}
+		
+		for(int i = 0; i < 1000; i++) {
 		CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> assignment, service)
-						 .thenApply(Assignment8 -> Assignment8.getNumbers())
+						 .thenApply(Assignment8::getNumbers)
 						 .thenAccept(newList -> outputList.addAll(newList));
 		
 		tasks.add(task);
@@ -25,12 +34,32 @@ public class Assignment8Application {
 		while(tasks.stream()
 			.filter(CompletableFuture::isDone)
 			.count() < 1000) {
-		
+
 		}
 		
 		String message = "Done. List size: ";
 		System.out.println(message + outputList.size());
-	}
 		
+		Map<Integer, Integer> outputMap = new HashMap<>();
+		
+		for(Integer output : outputList) {
+			
+			if (outputMap.containsKey(output)) {
+				outputMap.put(output, outputMap.get(output) + 1);
+			}
+			
+			else if (!outputMap.containsKey(output)) {
+				outputMap.put(output, 1);
+			}
+		}
+		
+		System.out.println("\nNumber of Times Each Unique Number Appears\n-------------------------------\n");
+		printToConsole(outputMap);
+	}
 
+	private static void printToConsole(Map<Integer, Integer> outputMap) {
+		
+		for (Map.Entry<Integer, Integer> entry : outputMap.entrySet())
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+	}
 }
